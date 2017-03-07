bf75458710cc94376fa053af1b563f8c2eb534fe
tomdobbelaere <tom.dobbelaere@outlook.com>
2017-03-07 14:16:08 +0100

    Working on the fly classification'

--- HBotJava/src/Program.java
+++ HBotJava/src/Program.java
@@ -1,15 +1,31 @@
 import weka.classifiers.Evaluation;
 import weka.classifiers.trees.J48;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
 import weka.core.Instances;
+import weka.core.converters.ConverterUtils;
 import weka.core.converters.ConverterUtils.DataSource;
+
+import java.util.Enumeration;
 
 /**
  * Created by tomdo on 7/03/2017.
  */
 public class Program {
-    public static void main(String[] args) throws Exception {
-        DataSource source = new DataSource("C:/Users/tomdo/Documents/Weka datasets/reddithelp.arff");
-        Instances data = source.getDataSet();
+    public static void main(String[] args) {
+        DataSource source = null;
+        try {
+            source = new DataSource("C:/Users/tomdo/Documents/Weka datasets/reddithelp.arff");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Instances data = null;
+        try {
+            data = source.getDataSet();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         if (data.classIndex() == -1)
         {
@@ -20,12 +36,67 @@ public class Program {
         System.out.println("Training J48 tree...");
 
         J48 tree = new J48();
-        tree.buildClassifier(data);
+        try {
+            tree.buildClassifier(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         System.out.println("J48 classifier ready!");
 
-        Evaluation eval = new Evaluation(data);
-        eval.evaluateModel(tree, data);
+        Evaluation eval = null;
+        try {
+            eval = new Evaluation(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            eval.evaluateModel(tree, data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         System.out.println(eval.toSummaryString("\nResults\n======\n", false));
+
+        System.out.println("WI " + data.attribute("a").index());
+
+        DenseInstance di = new DenseInstance(data.numAttributes());
+        di.setDataset(data);
+        String test = "i like cookies";
+
+        Enumeration<Attribute> ea = data.enumerateAttributes();
+
+        while (ea.hasMoreElements()) {
+            Attribute next = ea.nextElement();
+            System.out.println(next.name());
+            di.setValue(next.index(), 0);
+        }
+
+        test = test.toLowerCase();
+        for (String s : test.split(" ")) {
+            if (data.attribute(s) != null) {
+                di.setValue(data.attribute(s), 1);
+                System.out.println("Set value 1 for " + s);
+            }
+        }
+
+        System.out.println(tree.toString());
+
+        //di.setValue();
+        double cl = 0;
+        try {
+            di.setClassValue(tree.classifyInstance(di));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Instance class is: " + di.classValue());
+
+        if (di.classValue() > 0.5) {
+            System.out.println("Result for '" + test + "' is suicidal");
+        } else {
+            System.out.println("Result for '" + test + "' is not suicidal");
+        }
     }
 }
